384661d882ca26f98f0ea26b277d39d19c96995e
NastasiaAver <nastenka_napoli@rambler.ru>
2024-08-22 12:09:24 +0300

    Add files via upload

---
+++ Module_2_5.py
@@ -0,0 +1,16 @@
+
+n = input("Введите кол-во строк: " )
+m = input("Введите кол-во столбцов: ")
+value = input("Введите символ: ")
+print('Строк:', n, ", Столбцов:", m, ', Cимвол:', value)
+matrix = []
+get_matrix = []
+for i in range(0,int(n)):
+           print((value)* int(m))
+           matrix.append(value)
+for j in range(0,int(m)):
+               get_matrix.append(matrix)
+print(get_matrix)
+
+
+
